010c0ae46c5c090b8d76073d03b853b98069ffab
Tom Ashworth <tom@phuu.net>
2013-04-29 21:09:21 +0100

    Initial commit.

---
+++ example.js
@@ -0,0 +1,12 @@
+var ps = require('./');
+
+console.log('"%s"', ps.truncateToWord('hello world, this is the last word '));
+console.log('"%s"', ps.truncateToWord('hello world, this is the last wo'));
+console.log('"%s"', ps.truncateToWord('hello world, this is fab'));
+
+
+console.log();
+var post = 'Hello world, this is a long post that I wish to be split into many sub-posts. It\'s long, which is useful becuse it needs to be. Longer and longer it goes. Yesh.';
+console.log(ps.splitAtLength(post, 40));
+console.log();
+console.log(ps.segment(post, 50));
---
+++ test/test.js
@@ -0,0 +1,51 @@
+var ps = require('../'),
+    t = require("tap");
+
+t.test('trucation', function (t) {
+  t.equal(
+    ps.truncateToWord('hello world, this is the last word '),
+    'hello world, this is the last word');
+  t.equal(
+    ps.truncateToWord('hello world, this is the last wo'),
+    'hello world, this is the last');
+  t.equal(
+    ps.truncateToWord('hello world, this is the'),
+    'hello world, this is');
+  t.end();
+});
+
+t.test('splitAtLength', function (t) {
+  var original, res;
+
+  original = 'hello world, this is the last word ';
+  res = ps.splitAtLength(original, 15);
+  t.ok(res.string.length <= 15);
+  t.equal(res.string + res.remaining, original);
+
+  original = 'a longer post with #hashtags and @mentions in important places';
+  res = ps.splitAtLength(original, 15);
+  t.ok(res.string.length <= 15);
+  t.equal(res.string + res.remaining, original);
+
+  original = 'hello world, this is the last word ';
+  res = ps.splitAtLength(original, 15);
+  t.ok(res.string.length <= 15);
+  t.equal(res.string + res.remaining, original);
+  t.end();
+});
+
+t.test('segment', function (t) {
+  var original, res;
+
+  original = 'a longer post with #hashtags and @mentions in important places';
+  res = ps.segment(original, 15);
+  console.log(res);
+  t.ok(res.length >= 3);
+
+  original = 'an even longer post with #hashtags and @mentions in important places, as well as http://phuu.net links and – some – nice – punctuation there lads.';
+  res = ps.segment(original, 50);
+  console.log(res);
+  t.ok(res.length >= 2);
+
+  t.end();
+});
